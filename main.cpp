0890324243fd4dbbb0d852a080f857c6652787b4
Grigore Alexandra <grigorealexandra234@gmail.com>
2026-03-25 19:04:46 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,4 @@
-/*
-Acest program simulează procesul de admitere la facultate.
-
-Pentru fiecare candidat se introduc numele și prenumele,
-vârsta,media de bacalaureat, materia examenului de admitere și nota obținută la examen. Datele pot fi preluate manual de
-la tastatură sau automat dintr-un fișier, unde fiecare linie conține informațiile unui candidat.
-Programul utiizeaza clasele Candidat, Facultate, Examen si Admitere.
-
-Programul gestionează întregul flux de admitere: permite adăugarea candidaților, verifică dacă există o facultate
-corespunzătoare materiei examenului, calculează media de admitere in functie de nota obtinuta la examenul de admitere si
-media de la bac și validează datele astfel încât notele să fie corecte. În funcție de media minimă a facultății și de
-condițiile obligatorii de promovare, programul stabilește dacă fiecare candidat este admis sau respins.
-
-La final, programul reține doar candidații validați, împreună cu facultatea corespunzătoare, media de admitere și
-rezultatul (admis/respins).
-*/
+//Acest program simulează procesul de admitere la facultate.
 
 #include <iostream>
 #include <cstring>
